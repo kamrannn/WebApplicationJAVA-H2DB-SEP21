cc0d61ab50d1ece1949d2429809f37dc93cc10f0
kamranabbasi <rajakamran737@gmail.com>
2021-09-22 09:28:27 +0500

    Listing all the trainees

--- src/main/java/com/example/firstwebapplicationjava/DatabaseConnection.java
+++ src/main/java/com/example/firstwebapplicationjava/DatabaseConnection.java
@@ -4,15 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection  {
+public class DatabaseConnection {
     //Establishing the connection with H2 Database
     public static Connection getDbConnection() throws SQLException {
-        Connection conn =null;
-        try{
+        Connection conn = null;
+        try {
             Class.forName("org.h2.Driver");
             conn = DriverManager.getConnection("jdbc:h2:~/test;", "sa", "");
 //            System.out.println("Connection is Successfully Created");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Connection not established");
         }
         return conn;
--- src/main/java/com/example/firstwebapplicationjava/TraineesServlet.java
+++ src/main/java/com/example/firstwebapplicationjava/TraineesServlet.java
@@ -1,12 +1,14 @@
 package com.example.firstwebapplicationjava;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "ListOfUsers", value = "/TraineesList")
 public class TraineesServlet extends HttpServlet {
